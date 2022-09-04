e06170d6677261a9427785eae62e8b49dc07761b
prabhalp2001@gmail.com <prabhasanthiya2001@gmail.com>
2022-09-04 19:42:18 +0530

    Assignment

---
+++ revNum.java
@@ -0,0 +1,18 @@
+public class revNum
+{
+ public static void main(String[] args)
+ {
+   int num = 1234;
+   int revNum = 0;
+   int digit;
+
+   while(num !=0)
+   {
+     digit = num % 10;
+     revNum = revNum *10 + digit;
+     num = num / 10;
+   }
+     System.out.println("Reverse Number:" +revNum);
+ }
+}
+     
